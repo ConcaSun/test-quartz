8ece7df1210776463de247aa8f70f586552a01f0
suntg <suntg@usoftchina.com>
2018-10-10 10:07:53 +0800

    Initial commit

---
+++ src/main/java/com/usoft/suntg/quartz/TestQuartzApplication.java
@@ -0,0 +1,17 @@
+package com.usoft.suntg.quartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 主启动程序
+ * @author suntg
+ * @date 2018年10月10日10:05:30
+ */
+@SpringBootApplication
+public class TestQuartzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestQuartzApplication.class, args);
+	}
+}
