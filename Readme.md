b32818e4aa7ca97b8745f26819377ca896ef0ba6
suntg <suntg@usoftchina.com>
2018-10-10 14:36:12 +0800

    add readme file

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# test-quartz
+spring-boot 集成 quartz实现定时任务管理系统的测试项目
+
+## 项目说明
+
+## 注意事项
---
+++ src/main/java/com/usoft/suntg/quartz/service/SampleService.java
@@ -0,0 +1,20 @@
+package com.usoft.suntg.quartz.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * sampleService
+ * @author suntg
+ * @date 2018年10月10日11:08:08
+ */
+@Service
+public class SampleService {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public void sayHello() {
+        logger.info("Hello world.");
+    }
+}
